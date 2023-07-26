13403a584787e0e46b3eecfba61a9f492c6fd750
vu.com <vu.le.bui.quoc@gmail.com>
2023-07-26 17:30:28 +0700

    Updated signals and slots

---
+++ 02_Signals_and_Slots/signals_and_slots_01d.py
@@ -0,0 +1,28 @@
+import sys
+from PyQt6.QtWidgets import (
+  QApplication,
+  QMainWindow,
+  QPushButton,
+) 
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.button_is_checked = True
+        self.setWindowTitle("My App")
+        self.button = QPushButton("Press Me!") 
+        self.button.setCheckable(True)
+        self.button.released.connect(
+        self.the_button_was_released
+        ) 
+        self.button.setChecked(self.button_is_checked)
+        # Set the central widget of the Window.
+        self.setCentralWidget(self.button)
+    def the_button_was_released(self):
+        self.button_is_checked = self.button.isChecked() 
+        print(self.button_is_checked)
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
