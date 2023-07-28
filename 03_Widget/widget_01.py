b9d9f9a267c3cdd8f9027bcc9f6c2790aad030bd
Vu Le Bui Quoc <vu.le.bui.quoc@gmail.com>
2023-07-28 22:34:00 +0700

    Updated keyword and widget01

---
+++ 03_Widget/widget_01.py
@@ -0,0 +1,27 @@
+import sys
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My App")
+        # create label named widget
+        widget = QLabel("Hello")
+        # set variable name font for the font setup
+        font = widget.font()
+        # set font to size 30 
+        font.setPointSize(30)
+        # assign font setting to label
+        widget.setFont(font)
+        # set alignment for the label
+        # alignmentflag 
+        widget.setAlignment(
+        Qt.AlignmentFlag.AlignHCenter
+        | Qt.AlignmentFlag.AlignVCenter
+        ) 
+        self.setCentralWidget(widget)
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
